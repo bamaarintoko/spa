d96cfde7db7e67307d7a66b2d892dce740cd25ef
Yosafat Bama <yosafatbama.arintoko@gmail.com>
2018-04-13 23:20:59 +0700

    add react scroll

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,22 +1,57 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Particles_ from 'react-particles-js'
-import { Navbar, NavItem, Icon } from 'react-materialize'
+import { Navbar, NavItem, Icon } from 'react-materialize';
+import { Link, DirectLink, Element, Events, animateScroll as scroll, scrollSpy, scroller } from 'react-scroll'
 // import styles from './wrap.css'
 // import './a.css'
 class PageHome extends React.Component {
-    render() {
-        return (
-            <div style={{height: '100vh',
-            width: '100vw',backgroundColor:'#212121'}}>
+	constructor(props) {
+		super(props);
+		this.scrollToTop = this.scrollToTop.bind(this);
+	}
+	scrollToTop() {
+		scroll.scrollToTop();
+	}
+	componentDidMount() {
 
-                <Particles_
+		Events.scrollEvent.register('begin', function () {
+			console.log("begin", arguments);
+		});
+
+		Events.scrollEvent.register('end', function () {
+			console.log("end", arguments);
+		});
+
+	}
+	componentWillUnmount() {
+		Events.scrollEvent.remove('begin');
+		Events.scrollEvent.remove('end');
+	}
+	render() {
+		return (
+			<div>				
+				<Navbar brand='malaskoding.com' right style={{ backgroundColor: '#212121', border: 0 }}>
+					<Link style={{ display: 'inline-block',}} activeClass="active" to="firstInsideContainer" smooth={true} duration={250} containerId="containerElement" >Home</Link>
+					<Link style={{ display: 'inline-block',}} activeClass="active" to="secondInsideContainer" smooth={true} duration={250} containerId="containerElement">Our Team</Link>
+					<NavItem href='#'>Our Story</NavItem>
+					<NavItem href='#'>About</NavItem>
+				</Navbar>
+				<Element id="containerElement" style={{
+					position: 'relative',
+					height: (window.innerHeight-64)+'px',
+					width: '100%',
+					overflow: 'scroll',
+				}}>
+					<Element name="firstInsideContainer" style={{
+						height: '100vh',
+						width: '100vw',
+						backgroundColor:'#212121'
+					}}>
+					<Particles_
 					style={{
-						width: '100%', position: "fixed",
 						top: 0,
 						left: 0,
-						width: "100%",
-						height: "100%"
 					}}
 					params={{
 						particles: {
@@ -45,15 +80,20 @@ class PageHome extends React.Component {
 							}
 						}
 					}} />
-                <Navbar brand='malaskoding.com' right style={{ backgroundColor: '#212121', border: 0 }}>
-                    <NavItem href='#'>Home</NavItem>
-                    <NavItem href='#'>Our Team</NavItem>
-                    <NavItem href='#'>Our Story</NavItem>
-                    <NavItem href='#'>About</NavItem>
-                </Navbar>
-            </div>
-        );
-    }
+						first element inside container
+          			</Element>
+
+					<Element name="secondInsideContainer" style={{
+						height: '100vh',
+						width: '100vw',
+						backgroundColor:'blue'
+					}}>
+						second element inside container
+          			</Element>
+				</Element>
+			</div>
+		);
+	}
 }
 
 export default PageHome;
